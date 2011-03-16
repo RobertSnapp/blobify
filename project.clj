b19e7a9b91b1eb670e0fdf1b92b84583eb8b7127
Robert Snapp <Robert.Snapp@uvm.edu>
2011-03-16 11:59:11 -0400

    Added three jars from the fiji project to the lein's list of dependencies in project.clj. These allow the use of ImageJ's ImageProcessor class, and the LSM toolbox.

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject clj-ctree "0.1"
   :description "A prototype of the component tree datastructure and related tools for analyziing grayscale voxel arrays."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-				 [org.clojure/clojure-contrib "1.2.0"]]
+  :dependencies [[fiji-local/ij "1.0"]
+                 [fiji-local/LSM_Toolbox "4.0g"]
+                 [fiji-local/LSM_Reader "4.0g"]
+                 [org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :aot [ clj-ctree.core
 		 clj-ctree.image
