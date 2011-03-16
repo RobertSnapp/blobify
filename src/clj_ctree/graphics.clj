de935e54e115ba07e92179e4ae01970c10482fee
Robert Snapp <Robert.Snapp@uvm.edu>
2011-03-16 11:38:57 -0400

    Created module clj-ctree.graphics, which encapsulates functions that support the graphical display of multidimensional images, and component trees.

---
+++ src/clj_ctree/graphics.clj
@@ -0,0 +1,65 @@
+;;; Graphical routines and utilities
+
+(ns clj-ctree.graphics
+  (:import (java.awt.image BufferedImage)
+           (java.awt Color Graphics Dimension)
+           (javax.swing JPanel JFrame JLabel))
+  (:use [clojure.contrib.math :only (abs)]))
+
+(defn hsv-to-rgb
+  "Returns a vector that contains the red, green and blue color components for the color
+specified by its hue, saturation and value."
+  [h s v]
+  (let [c (* v s)                       ; chroma
+        hp (/ (mod h 360) 60)           ; hp is between 0 and 6
+        x (* c (- 1 (abs (- (mod hp 2) 1))))
+        m (- v c)
+        [r g b] (case (int hp)
+                      0 [c x 0]
+                      1 [x c 0]
+                      2 [0 c x]
+                      3 [0 x c]
+                      4 [x 0 c]
+                      5 [c 0 x]
+                      [0 0 0])]
+    (vec (map #(int (* 255 (+ % m))) [r g b]))))
+
+(defn get-indexed-rgb
+  "Generates a random color map that can be used for labeling components. The argument k should
+be a non-negative integer."
+  [k]
+  (let [n (+ k 1)
+        h (* 360 (mod (* n (Math/sqrt 0.014)) 1))
+        r (mod (* n (Math/sqrt 1.5)) 1)
+        s (+ 0.5 (* 0.5 r))
+        t (mod (* n (Math/sqrt 5.0)) 1)
+        v (+ 0.5 (* 0.5 t))]
+    (hsv-to-rgb h s v)))
+
+(def max-height 1024)
+(def max-width 1600)
+(defn render-indexed-rgb
+  "Display the leading indexed colors in a rectangular tiled array."
+  [cols rows]
+  (let [frame (JFrame. "Indexed Colors (RGB)")
+        ; scale (min (/ max-side (max cols rows)) 40)
+        scale (min (/ max-height rows) (/ max-width cols) 40)
+        buffer (new BufferedImage
+                    (* scale cols)
+                    (* scale rows)
+                    BufferedImage/TYPE_INT_RGB)
+        canvas (proxy [JLabel] []
+                 (paint [g] (.drawImage g buffer 0 0 this)))
+        graphics (.createGraphics buffer)]
+    (doseq [x (range cols)
+            y (range rows)]
+      (let [n (+ x (* cols y))
+            [r g b] (get-indexed-rgb n)]
+        (doto graphics
+          (.setColor (new Color r g b))
+          (.fillRect (* x scale) (* y scale) scale scale))))
+    
+    (.add frame canvas)
+    (.setSize frame (new Dimension (* scale cols) (+ 20 (* scale rows))))
+    (.show frame))
+  )
